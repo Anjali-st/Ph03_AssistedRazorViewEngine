caa7e7144a7ca8bc91e0acd959ee244e5758a2a9
Anjali-st <anjali90908080@gmail.com>
2023-09-13 22:22:47 +0530

    RazorViewEngine

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sec_04Razor_View_Engine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
